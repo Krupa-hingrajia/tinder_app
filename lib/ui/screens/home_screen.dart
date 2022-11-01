7fb8058eebb13c439a4a6320822ce13b7ccf9855
Cirkledeveloper <109329812+krupaHingrajiya@users.noreply.github.com>
2022-11-01 15:58:09 +0530

    changes in UI

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,6 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:tinder_app_new/core/view_model/base_view.dart';
 import 'package:tinder_app_new/core/view_model/screens_view_model/home_screen_view_model.dart';
@@ -61,50 +58,6 @@ class _HomeScreenState extends State<HomeScreen> {
       );*/
     }, onModelReady: (model) {
       this.model = model;
-      getFirebaseImageFolder();
     });
-  }
-
-  Future getData() async {
-    try {
-      await downloadURLExample();
-      return downloadURL;
-    } catch (e) {
-      debugPrint("Error - $e");
-      return null;
-    }
-  }
-
-  Future getFirebaseImageFolder() async {
-    final storageRef = FirebaseStorage.instance.ref().child('images');
-    storageRef.listAll().then((result) {
-      print("result is $result");
-    });
-  }
-
-  Future<void> downloadURLExample() async {
-    downloadURL = await FirebaseStorage.instance.ref().child("Sample.png").getDownloadURL();
-    print("*************************${downloadURL.toString()}");
-    //print("*************************${ref.getDownloadURL()}");
-    // return ref.getDownloadURL();
-  }
-
-  Future loadImage() async {
-    //current user id
-    final _userID = FirebaseAuth.instance.currentUser!.uid;
-
-    //collect the image name
-    DocumentSnapshot variable = await FirebaseFirestore.instance.collection('data_user').doc('user').collection('personal_data').doc(_userID).get();
-
-    //a list of images names (i need only one)
-    var _file_name = variable['path_profile_image'];
-
-    //select the image url
-    Reference ref = FirebaseStorage.instance.ref().child("images_1.jpeg");
-
-    //get image url from firebase storage
-    var url = await ref.getDownloadURL();
-    print('url: ' + url);
-    return url;
   }
 }
