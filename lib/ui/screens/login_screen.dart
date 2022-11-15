6a578b35dd369f308ee9a97a1c5b3181185bbbd4
cirkleDeveloper4 <107621731+cirkleDevloper4@users.noreply.github.com>
2022-11-15 09:24:34 +0530

    Update login_screen.dart

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tinder_app_new/core/routing/routes.dart';
 import 'package:tinder_app_new/ui/widget/custom_btn.dart';
 import 'package:tinder_app_new/core/view_model/base_view.dart';
 import 'package:tinder_app_new/ui/widget/custom_text_field.dart';
 import 'package:tinder_app_new/core/constant/image_constant.dart';
 import 'package:tinder_app_new/core/constant/text_style_constant.dart';
-import 'package:tinder_app_new/core/routing/routes.dart';
-import 'package:tinder_app_new/core/view_model/base_view.dart';
-import 'package:tinder_app_new/ui/widget/custom_btn.dart';
-import 'package:tinder_app_new/ui/widget/custom_text_field.dart';
 
+import '../../core/constant/color_constant.dart';
 import '../../core/view_model/screens_view_model/login_screen_view_model.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -55,7 +51,9 @@ class _LoginScreenState extends State<LoginScreen> {
                             validator: (value) {
                               String pattern = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
                               RegExp regex = RegExp(pattern);
-                              return value == null || value.isEmpty || !regex.hasMatch(value) ? 'Enter valid email address' : null;
+                              return value == null || value.isEmpty || !regex.hasMatch(value)
+                                  ? 'Enter valid email address'
+                                  : null;
                             }),
                         SizedBox(height: MediaQuery.of(context).size.height * 0.035),
                         customTextField(
@@ -97,7 +95,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               if (model.formKey.currentState!.validate()) {
                                 model.loginCircular = false;
                                 setState(() {});
-                                model.signInssUser(context: context);
+                                model.signInUser(context: context);
                                 model.getUserDetail();
                                 model.loginCircular = true;
                                 setState(() {});
